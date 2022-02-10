49a10aa2f794960543e9656560273be48ac9bbe3
Scott Everett <burningcupidxxx@gmail.com>
2022-02-10 02:44:22 -0800

    fill homepage template with content

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { User, Post } = require("../models");
+
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attriutes: ["username"]
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.toJSON());
+
+        res.render("homepage", { posts });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
