23ae7205caf1288c72160b7c9ecc96489b954865
Scott Everett <burningcupidxxx@gmail.com>
2022-02-13 10:09:06 -0800

    create get route for all posts, post route for creating new post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,4 +1,39 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
-router.post
+router.get('/', async (req, res) => {
+    const postData = await Post.findAll({
+        attributes: ['id', 'title', 'content'],
+
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'content', 'post_id', 'user_id'],
+            
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+    ]
+    })
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
+
+        res.json(newPost);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+module.exports = router;
---
+++ public/js/newpost.js
@@ -0,0 +1,7 @@
+const newPostHandler = (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#new-post-title').value;
+    const content = document.querySelector('#new-post-content').value;
+    
+}
