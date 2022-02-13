2773f5ba93d4a8d02d7f90047d4c2bd18686f13e
Scott Everett <burningcupidxxx@gmail.com>
2022-02-13 05:41:31 -0800

    create post route for creating new user, FE logic for signup form

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,11 +1,38 @@
 const router = require('express').Router();
 const { User } = require("../../models");
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const newUser = await User.create({
-            ...req.body,
-            user_id: req.session.user_id,
-        })
+       const userData = await User.create({
+           username: req.body.username,
+           email: req.body.email,
+           password: req.body.password,
+       });
+       
+       req.session.save(() => {
+           req.session.user_id = userData.id;
+           req.session.logged_in = true;
+
+           res.status(200).json(userData);
+       })
+    } catch (err) {
+        res.status(400).json(err);
+        console.log("USER CREATE ERR");
     }
-})
+});
+
+
+
+
+
+
+// router.post("/", async (req, res) => {
+//     try {
+//         const newUser = await User.create({
+//             ...req.body,
+//             user_id: req.session.user_id,
+//         })
+//     }
+// })
+
+module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -19,6 +19,32 @@ const loginFormHandler = async (event) => {
     }
 };
 
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#signup-username-input').value.trim();
+    const email = document.querySelector('#signup-email-input').value.trim();
+    const password = document.querySelector('#signup-password-input').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('signup fail');
+        }
+    }
+};
+
 document
-  .querySelector('.login-form')
+  .querySelector('#form-login')
   .addEventListener('submit', loginFormHandler);
+
+  document
+    .querySelector('#form-signup')
+    .addEventListener('submit', signupFormHandler);
